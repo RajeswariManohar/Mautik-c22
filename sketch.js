54f35a2321f9afc440bd89dfc024dd9fb2c0479e
RajeswariManohar <71829809+RajeswariManohar@users.noreply.github.com>
2020-11-27 15:29:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+const Engine=Matter.Engine;
+const World=Matter.World;
+const Bodies=Matter.Bodies;
+
+var engine,world;
+var ball;
+function setup() {
+  createCanvas(800,400);
+  engine=Engine.create();
+  world=engine.world;
+
+  var ball_options={
+    isStatic:true
+  }
+
+  ball=Bodies.circle(200,100,60,ball_options);
+  World.add(world,ball);
+}
+
+function draw() {
+  background(0);  
+  Engine.update(engine);
+
+  circle(ball.position.x,ball.position.y,60);
+  
+}
